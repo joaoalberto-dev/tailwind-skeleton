7071ce7a67d17ecbac55908fa3e10c89cf7644d8
João Alberto <hey@joaoalberto.dev>
2024-11-19 22:10:16 -0300

    chore: format layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,18 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Tailwind Skeleton",
-  description: "Tailwind Skeleton",
+    title: "Tailwind Skeleton",
+    description: "Tailwind Skeleton",
 };
 
 type RootLayoutProps = Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>;
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <html lang="en">
-      <body className="antialiased">{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className="antialiased">{children}</body>
+        </html>
+    );
 }
