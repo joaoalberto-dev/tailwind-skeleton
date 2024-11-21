75446c89709e21ae0dc0a297f67e4176b3f7e1a8
João Alberto <hey@joaoalberto.dev>
2024-11-21 00:06:50 -0300

    chore: new canvas component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { AppSidebar } from "@/core/components/app-sidebar";
+import Canvas from "@/core/components/canvas";
 import { SidebarProvider, SidebarTrigger } from "@/core/components/ui/sidebar";
 
 export default function Home() {
     return (
         <SidebarProvider className="flex">
             <AppSidebar />
-            <main className="flex flex-1">
-                <SidebarTrigger />
+            <main className="relative flex flex-1">
+                <SidebarTrigger className="drop-shadow-lg relative p-4 bg-white m-4 z-10" />
+                <Canvas />
             </main>
         </SidebarProvider>
     );
---
+++ src/core/components/canvas/index.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+function Canvas() {
+    return (
+        <div
+            className="absolute inset-0 w-full h-full flex items-center justify-center bg-neutral-100 
+            [background-image:radial-gradient(#cbd5e1_1px,transparent_1px)] [background-size:16px_16px]"
+        >
+            Canvas
+        </div>
+    );
+}
+
+export default Canvas;
