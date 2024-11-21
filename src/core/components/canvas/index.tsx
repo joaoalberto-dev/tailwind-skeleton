70a7a96184fe94575f19ac6fcfc2c99695acf7d9
João Alberto <hey@joaoalberto.dev>
2024-11-21 00:42:26 -0300

    chore: copy elements on drag to canvas

--- src/core/components/canvas/index.tsx
+++ src/core/components/canvas/index.tsx
@@ -1,12 +1,33 @@
 "use client";
 
 function Canvas() {
+    function onDragOver(ev: React.DragEvent<HTMLDivElement>) {
+        ev.preventDefault();
+        ev.dataTransfer.dropEffect = "copy";
+    }
+
+    function onDrop(ev: React.DragEvent<HTMLDivElement>) {
+        const data = ev.dataTransfer.getData("text/html");
+
+        if (data) {
+            const element = document.getElementById(data)?.cloneNode(true);
+
+            if (element) {
+                ev.currentTarget.appendChild(element);
+            }
+        }
+    }
+
     return (
         <div
             className="absolute inset-0 w-full h-full flex items-center justify-center bg-neutral-100 
             [background-image:radial-gradient(#cbd5e1_1px,transparent_1px)] [background-size:16px_16px]"
         >
-            Canvas
+            <div
+                className="w-[300px] h-[300px] bg-white border border-neutral-200 rounded-md drop-shadow-xl"
+                onDrop={onDrop}
+                onDragOver={onDragOver}
+            />
         </div>
     );
 }
--- src/core/components/drag/draggable.tsx
+++ src/core/components/drag/draggable.tsx
@@ -7,10 +7,22 @@ type DraggableProps = {
     children: React.ReactNode;
 };
 
-function Draggable({ children }: DraggableProps) {
+function Draggable({ children, id }: DraggableProps) {
+    function onDragStart(event: React.DragEvent<HTMLDivElement>) {
+        event.dataTransfer.setData("text/html", event.currentTarget.id);
+        event.dataTransfer.effectAllowed = "copyMove";
+    }
+
     return (
         <div className="w-full flex items-center justify-center aspect-square bg-neutral-100 border border-neutral-200 rounded-md">
-            <div className="inline-block">{children}</div>
+            <div
+                id={id}
+                draggable
+                onDragStart={onDragStart}
+                className="inline-block"
+            >
+                {children}
+            </div>
         </div>
     );
 }
